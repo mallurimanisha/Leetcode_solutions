311d7e83f97b1ee003282edd517e9bb12e659b63
mallurimanisha <manisha79937@gmail.com>
2026-09-02 00:01:47 +0530

    Time: 0 ms (100%), Space: 42.2 MB (19.17%) - LeetHub

---
+++ 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int xorOperation(int n, int start) {
+        int res=start;
+        for(int i=1;i<n;i++){
+            start+=2;
+            res=res^start;
+        }
+        return res;
+    }
+}
